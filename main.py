6675641dfc9a40be3883c4f0ce113cef09c4dc7f
Julián Perez <66093948+ositoMalvado@users.noreply.github.com>
2024-07-25 10:25:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.add(ft.SafeArea(ft.Text("Te amo ana")))
 
 
 ft.app(main, view=ft.WEB_BROWSER)
