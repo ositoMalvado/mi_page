bc74fc8c649074b6f2bbcbc9c38478b2fec1d8f0
Julián Perez <66093948+ositoMalvado@users.noreply.github.com>
2024-07-25 21:08:10 -0300

    update sound

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import flet as ft
 from Tabs import *
 from Funciones import *
+
 import random
+
+# class Example(ft.Column):
+#         def __init__(self):
+#             super().__init__()
+#             self.audio1 = ft.Audio(
+#                 src="https://luan.xyz/files/audio/ambient_c_motion.mp3", autoplay=True
+#             )
+
+#             async def pause_audio(e):
+#                 await self.audio1.pause()
+
+#             self.controls = [
+#                 ft.Text(
+#                     "This is an app with background audio. Note: this example doesn't work in Safari browser."
+#                 ),
+#                 ft.ElevatedButton("Stop playing", on_click=pause_audio),
+#             ]
+
+#         # happens when example is added to the page (when user chooses the Audio control from the grid)
+#         def did_mount(self):
+#             self.page.overlay.append(self.audio1)
+#             self.page.update()
+
+#         # happens when example is removed from the page (when user chooses different control group on the navigation rail)
+#         def will_unmount(self):
+#             self.page.overlay.remove(self.audio1)
+#             self.page.update()
 
 def main(page: ft.Page):
 
@@ -62,9 +90,10 @@ def main(page: ft.Page):
             ],
             tab_alignment=ft.TabAlignment.CENTER,
             expand=True
-        )
+        ),
+        # Example(),
     )
 
 
 
-ft.app(main, assets_dir="assets")
+ft.app(main, assets_dir="assets", view=ft.AppView.WEB_BROWSER)
